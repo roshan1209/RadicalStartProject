35ded8d4c5de5546576aaa78ea532a26e696b063
roshan1209 <roshan1209britto@gmail.com>
2023-06-15 13:23:41 +0530

    first_commit

---
+++ CRUD Operation/client/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import UserTable from './UserTable/UserTable';
+
+function App() {
+  return (
+    <div className="app">
+      <UserTable/>
+    </div>
+  );
+}
+
+export default App;
